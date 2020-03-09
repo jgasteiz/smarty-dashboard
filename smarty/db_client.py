b06994731551e48f4ceb94fd82b9fcbd9c0231ec
Javi Manzano <javi.manzano.oller@gmail.com>
2020-03-09 18:58:54 +0000

    Add a very simple json file to be used as a database
    For storing the authentication token

---
+++ smarty/db_client.py
@@ -0,0 +1,18 @@
+import json
+
+
+class DBClient:
+    db_file_name = "db.json"
+
+    @classmethod
+    def get_db(cls):
+        try:
+            with open(cls.db_file_name, "r") as f:
+                return json.loads(f.read())
+        except FileNotFoundError:
+            return {}
+
+    @classmethod
+    def update_db(cls, db):
+        with open(cls.db_file_name, "w") as f:
+            f.write(json.dumps(db))
--- smarty/smarty_client.py
+++ smarty/smarty_client.py
@@ -1,8 +1,9 @@
+import json
 from decimal import Decimal as D
 
 import requests
 
-from . import settings
+from . import db_client, settings
 
 
 class UnableToInitializeSmartyClient(Exception):
@@ -23,13 +24,16 @@ class SmartyClient:
         self.email = settings.SMARTY_EMAIL
         self.password = settings.SMARTY_PASSWORD
 
-    def get_usage(self):
+    def get_usage(self, retry=True):
         """
         Get the remaining usage.
         """
         headers = {"Authorization": self._get_authorization_header()}
         response = requests.get(self.USAGE_URL, headers=headers)
-        if response.status_code != 200:
+        if retry and response.status_code == 401:
+            self._clear_token()
+            return self.get_usage(retry=False)
+        elif response.status_code != 200:
             raise UnableToGetUsage(f"{response.status_code} - Unable to get usage")
 
         response_json = response.json()
@@ -45,12 +49,27 @@ class SmartyClient:
         """
         Get the authorization header value.
         """
+        return f"Bearer {self._get_token()}"
+
+    def _get_token(self):
+        db = db_client.DBClient.get_db()
+        if "token" in db:
+            return db["token"]
         payload = {"auth": {"email": self.email, "password": self.password}}
         response = requests.post(self.AUTH_URL, json=payload)
         if response.status_code != 201:
             raise UnableToGetUsage(f"{response.content} - Unable to authenticate")
-
         response_json = response.json()
-
         token = response_json["data"]["attributes"]["jwt"]
-        return f"Bearer {token}"
+        self._save_token(token)
+        return token
+
+    def _save_token(self, token):
+        db = db_client.DBClient.get_db()
+        db["token"] = token
+        db_client.DBClient.update_db(db)
+
+    def _clear_token(self):
+        db = db_client.DBClient.get_db()
+        del db["token"]
+        db_client.DBClient.update_db(db)
